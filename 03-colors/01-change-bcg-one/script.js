8b24ce91784204e01b3613b61d3614ede5e461d2
dagjo90 <dan.gjonaj90@gmail.com>
2018-11-04 18:01:22 +0100

    fin série 3

---
+++ 03-colors/01-change-bcg-one/script.js
@@ -0,0 +1,30 @@
+/* becode/javascript
+ *
+ * /03-colors/01-change-bcg-one/script.js - 3.1: couleur de fond (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+
+
+ document.getElementById("red").addEventListener("click", red);
+ document.getElementById("green").addEventListener("click", green);
+ document.getElementById("yellow").addEventListener("click", yellow);
+ document.getElementById("blue").addEventListener("click", blue);
+
+ function red (){
+    document.body.style.backgroundColor = "red";
+}
+  function green (){
+    document.body.style.backgroundColor = "green";
+}
+  function yellow (){
+    document.body.style.backgroundColor = "yellow";
+}
+  function blue (){
+    document.body.style.backgroundColor = "blue";
+}
